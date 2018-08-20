d1e3fe3483479f3e1f838889f6d22e7d9639944e
mjdefrank <mjdefrank@gmail.com>
2018-08-19 20:51:32 -0400

    added some bootstrap

--- app/server.js
+++ app/server.js
@@ -1,10 +1,16 @@
+// Instantiate ExpressJS
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
     console.log('App listening on ' + PORT);
 });
+// Set middleware
+const bodyParser = require('body-parser');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.get('/', function(req, res, next) {
-    res.send('Great Success!');
+    res.sendfile('./public/home.html');
 })
